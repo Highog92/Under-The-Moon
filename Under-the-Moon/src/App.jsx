2756d23a8e3fdbd10c4f3baf05bcb645992811c5
Highog92 <Madsserritslev@gmail.com>
2023-08-10 11:01:29 +0200

    New Style Section Færdig

--- Under-the-Moon/src/App.jsx
+++ Under-the-Moon/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { NavBar } from './Components/NavBar/Navbar'
+import { NewStyleSection } from './Components/NewStyle/NewStyle'
 function App() {
 
-  return (
-    <>
-      <NavBar/>
+    return (
+      <>
+        <NavBar />
+        <NewStyleSection/>
 
-
-    </>
-  )
-}
+      </>
+    )
+  }
 
 export default App
